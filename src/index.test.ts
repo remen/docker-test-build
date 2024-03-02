1ec0f34b0fbbbff8ad909663228f71ce81c65fb9
Petter Remen <petter.remen@orbiapp.io>
2024-03-02 11:24:13 +0100

    prettier

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,18 @@
-import expect from "expect";
+import * as net from 'net';
 
-describe("Hello world", () => {
-  it("works", () => {
-    expect('foo').toBe('foo');
+describe('Hello world', () => {
+  it('works', () => {
+    const socket = new net.Socket({});
+    socket.connect(
+      { autoSelectFamily: false, port: 5431, host: 'localhost' },
+      () => {
+        console.log('Socket established');
+      },
+    );
+    socket.on('error', (err) => {
+      console.error(err);
+    });
+
+    socket.resetAndDestroy();
   });
-})
+});
